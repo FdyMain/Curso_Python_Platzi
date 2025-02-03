7ff5aec35e826c79d7ca0334e9110e35b5d8f87a
fautomationhome <fautomationhome@gmail.com>
2025-02-03 18:13:36 -0500

    class 6

---
+++ clase_6.py
@@ -0,0 +1,45 @@
+#Operadores numéricos
+a = 10
+b = 3
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+print("Módulo:", a % b)
+
+#Si el resultado es decimal, solo muestra el entero, elimina los decimales
+print("Parte entera de la División:", a // b)
+
+#Elevado al cubo
+print("Potenciación:", a ** b)
+
+#Shorcuts
+a += 2
+print(a)
+
+a *= 2
+print(a)
+
+#PEMDAS
+
+operation_1 = 2 + 3 * 4
+operation_2 = 2 + 3 * 4
+
+print(operation_1)
+print(operation_2)
+
+operation_3 = (2+3) * (4**2) // 8 - 1
+
+print(operation_3)
+
+#operadores boolean
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+
