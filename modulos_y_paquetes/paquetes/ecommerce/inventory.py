20b8b541111412c28accc450a48c9e95c614723f
fautomationhome <fautomationhome@gmail.com>
2025-07-17 16:25:08 -0500

    modules and packages

---
+++ modulos_y_paquetes/paquetes/ecommerce/inventory.py
@@ -0,0 +1,9 @@
+#Agrega un producto a inventario
+def add_product(product_name, stock):
+    print(f'Producto {product_name} agregando con {stock} unicades en total')
+
+
+#Elimina un producto del inventario
+def remove_product(product_name):
+    print(f'Producto {product_name} eliminado del inventario')
+
---
+++ modulos_y_paquetes/paquetes/main.py
@@ -0,0 +1,6 @@
+from ecommerce.inventory import add_product, remove_product
+from ecommerce.sales import process_sales
+
+add_product('Laptop', 10)
+remove_product('Laptop')
+process_sales('Laptop', 2)
