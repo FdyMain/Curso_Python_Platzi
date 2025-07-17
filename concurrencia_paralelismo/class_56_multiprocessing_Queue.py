12058ebe0ccd494f2652c27e9dce9a1bd0010a6d
fautomationhome <fautomationhome@gmail.com>
2025-07-17 16:04:33 -0500

    concurrencia y paralelismo

---
+++ concurrencia_paralelismo/class_56_multiprocessing_Queue.py
@@ -0,0 +1,22 @@
+import multiprocessing
+
+"""
+Usamos Queue para que el proceso secundario
+pueda pasar datos de vuelta al proceso principal.
+"""
+
+def calcular_cuadrado(numeros, cola):
+    for n in numeros:
+        cola.put(n * n)
+
+if __name__ == "__main__":
+    numeros = [1, 2, 3, 4, 5]
+    cola = multiprocessing.Queue()
+
+    proceso = multiprocessing.Process(target=calcular_cuadrado, args=(numeros, cola))
+    proceso.start()
+    proceso.join()
+
+    # Extraer resultados de la cola
+    while not cola.empty():
+        print(cola.get())
